22866b77c694a17cd77eb5a61b66ed2c5f17bc64
SK <microfixbg@gmail.com>
2025-05-30 22:22:28 +0300

    .
    .

--- GHR/IdentityServer/obj/Debug/net9.0/IdentityServer.AssemblyInfo.cs
+++ GHR/IdentityServer/obj/Debug/net9.0/IdentityServer.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af9845787cd0ae3597b49fd823771bcadae7db6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94f3ee954c35aa3ccdf5741158467c7e5a53427e")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
